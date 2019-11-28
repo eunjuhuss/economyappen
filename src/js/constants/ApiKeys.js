46bc9e532b986fc2d4dc234584a60593362880fe
eunjuhuss <eunjuhuss@hotmail.com>
2019-11-28 18:47:53 +0100

    Add firebaseconfig

--- src/js/constants/ApiKeys.js
+++ src/js/constants/ApiKeys.js
@@ -1,8 +1,12 @@
   export default {
     FirebaseConfig : {
-      apiKey: 'AIzaSyDcg7UHivITwlLsxE_IgY_l70UO5kDJ768',
-      authDomain: 'economyapp-83469.web.app',
-      databaseURL: 'https://economyapp-83469.firebaseio.com',
-      projectId: 'economyapp-83469'
+    apiKey: "AIzaSyC3vJR84nFlora-BIeiK4MVppUK_kcvzn0",
+    authDomain: "economyappen.firebaseapp.com",
+    databaseURL: "https://economyappen.firebaseio.com",
+    projectId: "economyappen",
+    storageBucket: "economyappen.appspot.com",
+    messagingSenderId: "758118324568",
+    appId: "1:758118324568:web:f3a287bd1e13368b60ed2d",
+    measurementId: "G-XD5ETZTR36"
     }
   }
