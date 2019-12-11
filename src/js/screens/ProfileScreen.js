532e29d0ba80eeaed88d0da4aa98852d43d181ef
eunjuhuss <eunjuhuss@hotmail.com>
2019-12-11 18:51:30 +0100

    Add style

--- src/js/screens/ProfileScreen.js
+++ src/js/screens/ProfileScreen.js
@@ -4,30 +4,22 @@ import {
   StyleSheet,
   View
 } from 'react-native';
+import { styles } from '../../styles/ProfileScreenStyles';
 
+class ProfileScreen extends React.Component {
 
-export default function ProfileScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-      >
-      </ScrollView>
-    </View>
-  );
-
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+            contentContainerStyle={styles.contentContainer}>
+        </ScrollView>
+      </View>
+    );
+  }
 }
-
 ProfileScreen.navigationOptions = {
   title: 'Profile',
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  }
-});
+export default ProfileScreen;
---
+++ src/styles/ProfileScreenStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../js/constants/Colors';
+import Dimensions from '../js/constants/Dimensions';
+import Fonts from '../js/constants/Fonts';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop: Dimensions.hp(30),
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.mainGreenColor
+  },
+    contentContainer: {
+    backgroundColor: Colors.mainBlackColor,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30
+  },
+})
