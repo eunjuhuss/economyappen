9a0da2984702226b53b41402a0bce86f542827ad
eunjuhuss <eunjuhuss@hotmail.com>
2019-12-17 10:53:15 +0100

    Replace view and placeholder

--- src/js/components/Calendar.js
+++ src/js/components/Calendar.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { 
+  Text,
+  View,
+} from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import LabelIcon from '../components/LabelIcon';
 import Fonts from '../constants/Fonts';
 import Colors from '../constants/Colors';
+import { styles } from '../../styles/AddScreenStyles';
 
-const Calendar = ({date, onDateChange}) => {
-
-    return (      
+const Calendar = ({placeHolder,date, onDateChange}) => {
+  return ( 
+    <View style={styles.labelInputBox}>
+      <Text style={styles.labelText}>
+        {placeHolder}
+      </Text>     
       <DatePicker
-        // iconComponent={
-          // <LabelIcon type={'calendar'} />
-        // }
         style={{width: '100%'}}
-        date={date!== ''? new Date() : null} //initial date from state
-        mode="date" //The enum of date, datetime and time
+        date={date!== ''? new Date() : null} 
+        mode="date"
         placeholder="Select Date"
         format="DD-MM-YYYY"
         minDate="01-01-2019"
@@ -22,26 +27,19 @@ const Calendar = ({date, onDateChange}) => {
         cancelBtnText="Cancel"       
             
         customStyles={{
-          // dateIcon: {
-          //   position: 'absolute',
-          //   left: 0,
-          //   top: 4,
-          //   marginLeft: 0,
-          // },
           dateInput: {
             borderWidth: 1,
             alignItems: 'center',        
           },
-            dateText: {
-              color: Colors.subGrayColor,
-              fontFamily: Fonts.subText,
-              fontSize: 16
-            }
+          dateText: {
+            color: Colors.subGrayColor,
+            fontFamily: Fonts.subText,
+            fontSize: 16
+          }
         }}
         onDateChange={onDateChange}
-      />      
-    )
+      /> 
+    </View>     
+  )
 }
-
 export default Calendar;
-
